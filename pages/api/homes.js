c572a96d16fb0fba9e2eef374b052f67ae4a7934
Danier Martinez <dmartinez@wawand.co>
2023-03-06 09:37:11 -0500

    creating homes and handling error

---
+++ pages/api/homes.js
@@ -0,0 +1,38 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req, res) {
+  const { method } = req;
+
+  switch (method) {
+    case "POST":
+      try {
+        const { image, title, description, price, guests, beds, baths } =
+          req.body;
+
+        const home = await prisma.home.create({
+          data: {
+            image,
+            title,
+            description,
+            price,
+            guests,
+            beds,
+            baths,
+          },
+        });
+
+        res.status(200).json(home);
+      } catch (e) {
+        res.status(500).json({ message: "Something went wrong" });
+      }
+
+      break;
+    default:
+      res.setHeader("Allow", ["POST"]);
+      res
+        .status(405)
+        .json({ message: `HTTP method ${req.method} is not supported` });
+  }
+}
