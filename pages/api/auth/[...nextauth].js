66796644ef1f07b8e863cc9a394b1b2e7e6a221f
Danier Martinez <dmartinez@wawand.co>
2023-03-08 16:34:48 -0500

    customizing the confirmation email

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -2,6 +2,45 @@ import NextAuth from 'next-auth';
 import EmailProvider from 'next-auth/providers/email';
 import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import { prisma } from '../../../server/db/client';
+import nodemailer from 'nodemailer';
+import * as path from 'path';
+import { readFileSync } from 'fs';
+import Handlebars from 'handlebars';
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_SERVER_HOST,
+  port: process.env.EMAIL_SERVER_PORT,
+  auth: {
+    user: process.env.EMAIL_SERVER_USER,
+    pass: process.env.EMAIL_SERVER_PASSWORD,
+  },
+  // secure: true,
+});
+
+const emailsDir = path.resolve(process.cwd(), 'emails');
+
+const sendVerificationRequest = async ({ identifier, url }) => {
+  const emailFile = readFileSync(path.join(emailsDir, 'confirm-email.html'), {
+    encoding: 'utf8',
+  });
+
+  const emailTemplate = Handlebars.compile(emailFile);
+  const result = await transporter.sendMail({
+    from: `"✨ SupaVacation" ${process.env.EMAIL_FROM}`,
+    to: identifier,
+    subject: 'Your sign-in link for SupaVacation',
+    html: emailTemplate({
+      base_url: process.env.NEXTAUTH_URL,
+      signin_url: url,
+      email: identifier,
+    }),
+  });
+
+  const failed = result.rejected.concat(result.pending).filter(Boolean);
+  if (failed.length) {
+    throw new Error(`Email(s) (${failed.join(', ')}) could not be sent.`);
+  }
+};
 
 export const authOptions = {
   pages: {
@@ -13,16 +52,8 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
-      from: process.env.EMAIL_FROM,
       maxAge: 10 * 60, // Magic links are valid for 10min only
+      sendVerificationRequest,
     }),
   ],
 };
